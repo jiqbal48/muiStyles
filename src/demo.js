f381ee8fc5b2d14e6a3171a61cae5b6de168a859
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-09-16 14:00:20 -0400

    bringing in buttonToggle

--- src/demo.js
+++ src/demo.js
@@ -1,8 +1,10 @@
 import React from "react";
 import MyButton from "./button";
 import MyRadios from "./radioButtons";
+import MyToggleButtons from "./toggleButton";
 
 export default function StyledComponents() {
   // return <MyButton>Styled Components</MyButton>;
-  return <MyRadios />;
+  // return <MyRadios />;
+  return <MyToggleButtons />;
 }
