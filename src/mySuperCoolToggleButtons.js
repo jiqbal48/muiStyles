47844119ea4550eaeabeb161727ce969255bc456
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-09-16 15:44:18 -0400

    its all coming together

--- src/mySuperCoolToggleButtons.js
+++ src/mySuperCoolToggleButtons.js
@@ -1,57 +1,83 @@
 import React from "react";
 import ToggleButton from "@material-ui/lab/ToggleButton";
 import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
-import { makeStyles } from "@material-ui/core/styles";
 import { styled } from "@material-ui/styles";
 
-const useStyles = makeStyles(theme => ({
-  toggleContainer: {
-    margin: theme.spacing(2, 0)
-  }
-}));
-
+// custom components
 const MyTogun = styled(ToggleButton)({
-  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-  border: 0,
+  border: "2px solid cornflowerblue",
   borderRadius: 3,
-  boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-  color: "white",
   height: 48,
-  padding: "0 30px"
+  padding: "0 30px",
+  margin: "0 15px"
+});
+const MyTogunGroup = styled(ToggleButtonGroup)({
+  display: "flex",
+  flexWrap: "wrap"
 });
 
+// time slots
 export default function MySuperCoolToggleButtons() {
+  // personal state/backyard using hooks
   const [timeSlot, chooseTimeSlot] = React.useState(1);
-
   const handleTimeSelection = (event, newAlignment) => {
     chooseTimeSlot(newAlignment);
   };
 
-  const classes = useStyles();
-
-  console.log("hi");
+  // template
   return (
-    <div className={classes.toggleContainer}>
-      <p>hi</p>
-      <ToggleButtonGroup
+    <div
+      style={{
+        width: "700px",
+        height: "300px",
+        padding: "30px",
+        border: "1px solid palevioletred",
+        display: "flex"
+      }}
+    >
+      <MyTogunGroup
         value={timeSlot}
         exclusive
         onChange={handleTimeSelection}
-        aria-label="text alignment"
+        aria-label="time slots"
       >
-        <MyTogun value={1} aria-label="left aligned">
+        <MyTogun value={1} aria-label="7:30am">
           <span>7:30am</span>
         </MyTogun>
-        <MyTogun value={2} aria-label="centered">
+        <MyTogun value={2} aria-label="8:30am">
           <span>8:30am</span>
         </MyTogun>
-        <MyTogun value={3} aria-label="right aligned">
+        <MyTogun value={3} aria-label="9:30am">
           <span>9:30am</span>
         </MyTogun>
-        <MyTogun value={4} aria-label="justified">
+        <MyTogun value={4} aria-label="10:30am">
           <span>10:30am</span>
         </MyTogun>
-      </ToggleButtonGroup>
+        <MyTogun value={5} aria-label="7:30am">
+          <span>7:30am</span>
+        </MyTogun>
+        <MyTogun value={6} aria-label="8:30am">
+          <span>8:30am</span>
+        </MyTogun>
+        <MyTogun value={7} aria-label="9:30am">
+          <span>9:30am</span>
+        </MyTogun>
+        <MyTogun value={8} aria-label="10:30am">
+          <span>10:30am</span>
+        </MyTogun>
+        <MyTogun value={9} aria-label="7:30am">
+          <span>7:30am</span>
+        </MyTogun>
+        <MyTogun value={10} aria-label="8:30am">
+          <span>8:30am</span>
+        </MyTogun>
+        <MyTogun value={11} aria-label="9:30am">
+          <span>9:30am</span>
+        </MyTogun>
+        <MyTogun value={12} aria-label="10:30am">
+          <span>10:30am</span>
+        </MyTogun>
+      </MyTogunGroup>
     </div>
   );
 }
